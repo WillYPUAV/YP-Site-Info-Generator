293e6f18bbcde2c85c332f79626cc49d66bea4e7
WillYPUAV <whalley@ypassociates.com>
2025-10-23 16:14:00 -0500

    Update Collin County link to ZIP format

--- county_flood_links.py
+++ county_flood_links.py
@@ -3,7 +3,7 @@ county_flood_links = {
     "Dallas":  "https://www.dropbox.com/scl/fi/t6ohze36h97ek85thyt8b/Dallas_County_FEMA_FLD.zip?rlkey=9fv9opzqv21kgn46pwt9najjw&st=vbfbnin2&dl=1",
     "Denton":  "https://www.dropbox.com/scl/fi/66ytkpholqrnsxdaqmbo8/Denton_County_FEMA_FLD.zip?rlkey=j7ls7tezrp2q15vji199aqpnf&st=w4sz8srw&dl=1",
     "Wise":    "https://www.dropbox.com/scl/fi/55t6v87kze3d3yepxymgc/Wise_County_FEMA_FLD.zip?rlkey=v93lsuwl8zvo0f60o1smvoazm&st=sdfpiruy&dl=1",
-    "Collin":  "https://www.dropbox.com/scl/fi/x58g1layk29f8z2j78hll/Collin_County_FEMA_FLD.shp?rlkey=68ebaxt2mxbbqrvaq1zj4xdsq&st=3ni4rlcl&dl=1",
+    "Collin":  "https://www.dropbox.com/scl/fi/jfru49it7ugl0lwpkies7/Collin_County_FEMA_FLD.zip?rlkey=bqbpftrlggeqzomoz7yingnsa&st=fwlg4qfj&dl=1",
     "Johnson": "https://www.dropbox.com/scl/fi/vo914joe5mgqcv7egu9oi/Johnson_County_FEMA_FLD.zip?rlkey=41j6ynytr2mwbshocbar6chmj&st=h4pg9xny&dl=1",
     "Kaufman": "https://www.dropbox.com/scl/fi/8dlwpc3qdbriy3vdam547/Kaufman_County_FEMA_FLD.zip?rlkey=xj7ux7vydo7wihifuv2se5wdp&st=hs3u287z&dl=1",
     "Parker":  "https://www.dropbox.com/scl/fi/8o55x2s8ex74il655x5zy/Parker_County_FEMA_FLD.zip?rlkey=nkgx7o3qm51rf58t406xuufb4&st=q3rld990&dl=1",
