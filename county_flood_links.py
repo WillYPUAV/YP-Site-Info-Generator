9cad813f96357f40ab4cc043c0057aca12115012
WillYPUAV <whalley@ypassociates.com>
2025-10-23 15:41:15 -0500

    Update flood links to new Dropbox URLs

---
+++ county_flood_links.py
@@ -0,0 +1,12 @@
+county_flood_links = {
+    "Tarrant": "https://www.dropbox.com/scl/fi/xxk439ukf6rpmr3v3ufva/Tarrant_County_FEMA_FLD.shp?rlkey=g6h0j8upcmvcqqb3uy832j6dy&st=p6owo7jq&dl=1",
+    "Dallas":  "https://www.dropbox.com/scl/fi/zvzlghaxxrzj9fdzi8hbx/Dallas_County_FEMA_FLD.shp?rlkey=xepmzd3n8uch3s02sch2yav29&st=mnv3j1iq&dl=1",
+    "Denton":  "https://www.dropbox.com/scl/fi/m6escs2wul5k8h6rzux9d/Denton_County_FEMA_FLD.shp?rlkey=b0rrcvbjnh40itoufn2fbbv9x&st=j6jmdqw7&dl=1",
+    "Wise":    "https://www.dropbox.com/scl/fi/z0uznpu2qd0t5qsr6unp7/Wise_County_FEMA_FLD.shp?rlkey=0n44420bw9jjxgz7k077b08es&st=09hiq4lb&dl=1",
+    "Collin":  "https://www.dropbox.com/scl/fi/x58g1layk29f8z2j78hll/Collin_County_FEMA_FLD.shp?rlkey=68ebaxt2mxbbqrvaq1zj4xdsq&st=3ni4rlcl&dl=1",
+    "Johnson": "https://www.dropbox.com/scl/fi/tybz6ug9p546ryeybbbs7/Johnson_County_FEMA_FLD.shp?rlkey=qev5c8mj5n9vjh7i5d9jnw85a&st=a3tc3dk6&dl=1",
+    "Kaufman": "https://www.dropbox.com/scl/fi/mw0qv3h8d30gv7nhucjgs/Kaufman_County_FEMA_FLD.shp?rlkey=cid3sx0rb8h2hmdmrr6pzset8&st=ef6efgoy&dl=1",
+    "Parker":  "https://www.dropbox.com/scl/fi/e1vz7rldsmmpttbelu1xb/Parket_County_FEMA_FLD.shp?rlkey=b211eflhz1o8awfs7vn3az0mb&st=yjmys5kp&dl=1",
+    "Rockwall":"https://www.dropbox.com/scl/fi/j5ysnztwz66dta21whywi/Rockwall_County_FEMA_FLD.shp?rlkey=7fhnlbav3kolbudexxms7l4jg&st=zy1arjq8&dl=1",
+    "Ellis":   "https://www.dropbox.com/scl/fi/a6oyxlmdxhs9k79q7vcpt/Ellis_County_FEMA_FLD.shp?rlkey=t4or35jwa83mr5dchio46o1hh&st=svdq90ve&dl=1",
+}
